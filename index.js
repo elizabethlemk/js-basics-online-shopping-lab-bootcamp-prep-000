715d5f474fb7c237db621aac251eb73deeaac6b3
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-11-28 01:18:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,38 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var items = new Object ({
+    itemName: [item],
+    itemPrice: Math.floor((Math.random() * 100) + 1)
+  })
+  cart.push(items)
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+    if(cart.length === 1){
+      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+    }else if (cart.length > 1){
+      let string = `In your cart, you have `
+      for (let i = 0; i < cart.length-1; i++){
+        string += `${cart[i].itemName} at $${cart[i].itemPrice}, `;
+      //  console.log(string);
+        if(i = cart.length){
+          return string += `and ${cart[i].itemName} at $${cart[i].itemPrice}.`
+        }
+      }
+        //return string
+    }else{
+      return `Your shopping cart is empty.`
+  }
 }
 
 function total() {
-  // write your code here
+  var total = 0
+  for (let i = 0; i < cart.length; i++){
+    total += cart[i].itemPrice
+  }
+  return total
 }
 
 function removeFromCart(item) {
